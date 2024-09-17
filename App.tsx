3ee88854099e1542e5714325fc231e160267c46f
victoraugusto-jpg <victor.2022024077@alunocesul.com>
2024-09-16 21:54:30 -0300

    feat: crianção do componente Loading

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
+import { Loading } from "@components/Loading";
+import { Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import { Teams } from "@screens/Teams";
 import theme from "@theme/index";
+import { useFonts } from "expo-font";
 import { ThemeProvider } from "styled-components/native";
 
 export default function App() {
+    const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
     return (
         <ThemeProvider theme={theme}>
-            <Teams />
+            {fontsLoaded ? <Teams /> : <Loading />}
         </ThemeProvider>
     );
 }
